e6b0b0543d6098f73d660899d2042a5acdf10b4c
Luis Jaramillo Olate. <l.jaramillo02@ufromail.cl>
2026-05-22 15:16:22 -0400

    feat: cambios en el app.tsx añadir rutas de login y registro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
-  return (
-    <div>
-      <h1>ApexMetrics</h1>
-    </div>
-  )
+export default function App() {
+    return (
+
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/login" element={<Login />} />
+
+
+                <Route path="/register" element={<Register />} />
+
+
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App
